4091c33ff16b55725951a0375ebf6429a4aed5d5
Praveen <Praveen@DESKTOP-PPK9R37>
2017-09-27 14:29:12 -0400

    Lecture 3

---
+++ Vidly/Controllers/MoviesController.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections.Generic;
+using System.Web.Mvc;
+using Vidly.Models;
+using System.Data.Entity;
+using System.Linq;
+using System.Runtime.InteropServices;
+
+namespace Vidly.Controllers
+{
+    public class MoviesController : Controller
+    {
+        private ApplicationDbContext _context;
+
+        public MoviesController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+        }
+
+        //GET: movies/Details/id
+        public ActionResult Details(int id)
+        {
+            var movie = _context.Movies.Include(m => m.Genre).SingleOrDefault(m => m.Id == id);
+
+            if (movie == null)
+                return HttpNotFound();
+
+            return View(movie);
+        }
+
+        //GET: movies/Edit/id
+        public ActionResult Edit(int id)
+        {
+            return Content("id = " + id);
+        }
+
+        //GET: movies
+        public ActionResult Index()
+        {
+            var movies = _context.Movies.Include(m => m.Genre).ToList();
+
+            return View(movies);
+;        }
+
+        [Route("movies/released/{year:regex(\\d{4}):range( 2012, 2017)}/{month:regex(\\d{2}):range( 1, 12)}")]
+        //GET: movies/ByReleaseDate
+        public ActionResult ByReleaseDate(int year, byte month)
+        {
+            return Content(year + "/" + month);
+        }
+    }
+}
---
+++ Vidly/Migrations/201709271810459_AddChangesToGenreInCustomer.cs
@@ -0,0 +1,38 @@
+namespace Vidly.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AddChangesToGenreInCustomer : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Genres",
+                c => new
+                    {
+                        Id = c.Byte(nullable: false),
+                        Name = c.String(nullable: false, maxLength: 255),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+            CreateIndex("dbo.Movies", "GenreId");
+            AddForeignKey("dbo.Movies", "GenreId", "dbo.Genres", "Id", cascadeDelete: true);
+            DropColumn("dbo.Movies", "Genre");
+
+            Sql("INSERT INTO Genres (Id, Name) VALUES (1, 'Action')");
+            Sql("INSERT INTO Genres (Id, Name) VALUES (2, 'Thriller')");
+            Sql("INSERT INTO Genres (Id, Name) VALUES (3, 'Family')");
+            Sql("INSERT INTO Genres (Id, Name) VALUES (4, 'Romance')");
+            Sql("INSERT INTO Genres (Id, Name) VALUES (5, 'Comedy')");
+        }
+        
+        public override void Down()
+        {
+            AddColumn("dbo.Movies", "Genre", c => c.String(nullable: false));
+            DropForeignKey("dbo.Movies", "GenreId", "dbo.Genres");
+            DropIndex("dbo.Movies", new[] { "GenreId" });
+            DropTable("dbo.Genres");
+        }
+    }
+}

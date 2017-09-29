16adb5dbe60685a30298e1b940c9c3e386f75580
Praveen <Praveen@DESKTOP-PPK9R37>
2017-09-29 00:35:25 -0400

    Add AutoMapper

---
+++ Vidly/Dtos/CustomerDto.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using Vidly.Models;
+
+namespace Vidly.Dtos
+{
+    public class CustomerDto
+    {
+        public int Id { get; set; }
+        
+        public string Name { get; set; }
+
+        public bool IsSubscribedToNewsletter { get; set; }
+        
+        public byte MembershipTypeId { get; set; }
+
+        [Min18YearsIfAMember]
+        public DateTime? BirthDate { get; set; }
+    }
+}
